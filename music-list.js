9574346dc493260d65a801886cb7bffd3deedda9
urvashi-19 <0308urvashi@gmail.com>
2022-08-17 00:23:35 +0800

    my first commit

---
+++ music-list.js
@@ -0,0 +1,105 @@
+// To add more song, just copy the following code and paste inside the array
+
+//   {
+//     name: "Here is the music name",
+//     artist: "Here is the artist name",
+//     img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+//     src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+//   }
+
+//paste it inside the array as more as you want music then you don't need to do any other thing
+
+let allMusic = [
+    {
+      name: "Into Your Arms",
+      artist: "Witt Lowry",
+      img: "arms1.jpg",
+      src: "arms1"
+    },
+    {
+      name: "Butter",
+      artist: "Bts",
+      img: "butter.jpg",
+      src: "butter.mp3"
+    },
+    {
+      name: "Closer Song The Chainsmokers",
+      artist: "Chance The Rapper",
+      img: "closer.jpg",
+      src: "closer.mp3"
+    },
+    {
+      name: "Despacito",
+      artist: "Daddy Yankee and Luis Fonsi",
+      img: "descpacito.jpg",
+      src: "despacito.mp3"
+    },
+    {
+      name: "Shape Of You",
+      artist: "Ed Sheeran",
+      img: "shape.jpg",
+      src: "shape.mp3"
+    },
+    {
+      name: "Sugar and Brownies",
+      artist: "Dharia",
+      img: "sugar.jpg",
+      src: "suga.mp3"
+    },
+    {
+      name: "Harley-In-Hawaii",
+      artist: "Katy Perry",
+      img: "harley.jpg",
+      src: "harley.mp3"
+    },
+    {
+      name: "Joker BGM",
+      artist: "Dharani",
+      img: "joker.jpg",
+      src: "joker.mp3"
+    },
+    {
+      name: "Love Nwantiti",
+      artist: "Ckay",
+      img: "nwantiti.jpg",
+      src: "nwantiti.mp3"
+    },
+    {
+      name: "Thunder--Imagine",
+      artist: "Imagine Dragon",
+      img: "thunder.jpg",
+      src: "thunder.mp3"
+    },
+    {
+      name: "Lovely",
+      artist: "Billie Eilish and Khalid",
+      img: "lovely.jpg",
+      src: "lovely.mp3"
+    },
+    {
+      name: "Mi-Gente",
+      artist: "J Balvib and Willy William",
+      img: "mi.jpg",
+      src: "mi.mp3"
+    },
+    {
+      name: "Stay-Kid-Laroi",
+      artist: "Charlton Kenneth",
+      img: "stay.png",
+      src: "Stay-Kid-Laroi.mp3"
+    },
+    {
+      name: "Senorita",
+      artist: "Camila Cabello and Shawn Mendes",
+      img: "senorita.jpg",
+      src: "seno.mp3"
+    },
+    {
+      name: "One Kiss",
+      artist: "Dua Lipa",
+      img: "one.jpg",
+      src: "One Kiss.mp3"
+    },
+    
+  
+  ];
